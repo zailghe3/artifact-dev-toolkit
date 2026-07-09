3ce49ede090726d196a9ac17887d0dc5c3fb65e1
zailghe3 <zelhge3@gmail.com>
2026-07-09 14:27:23 +0100

    Configure Cloudflare OpenNext deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
